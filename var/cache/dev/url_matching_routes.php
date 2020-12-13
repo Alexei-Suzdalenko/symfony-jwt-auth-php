35eef270e3bb96399f1bfaa34c3488ff19776846
Alexey-Klintsy <xamman35@gmail.com>
2020-12-13 12:35:47 +0100

    first commit

---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/books' => [[['_route' => 'books', '_controller' => 'App\\Controller\\BooksController::index'], null, null, null, false, false, null]],
+        '/login' => [[['_route' => 'login', '_controller' => 'App\\Controller\\LoginController::index'], null, null, null, false, false, null]],
+        '/refresh' => [[['_route' => 'refresh', '_controller' => 'App\\Controller\\RefreshController::index'], null, null, null, false, false, null]],
+        '/register' => [[['_route' => 'register', '_controller' => 'App\\Controller\\RegisterController::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
